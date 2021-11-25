a84202f8cbb759ef03696efe658ca3711316ee3f
Samrit <hairaer02@gmail.com>
2021-11-25 14:17:11 +0300

    adding POJO Closes #2

---
+++ src/main/java/com/samrit/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.samrit.springpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/samrit/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.samrit.springpetclinic.model;
+
+public class Vet  extends  Person{
+}
