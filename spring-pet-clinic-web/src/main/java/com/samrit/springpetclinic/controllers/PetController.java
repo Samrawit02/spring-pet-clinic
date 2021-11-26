a5ffef55cb06a15ac05365caef75989085d6c4cd
Samrit <hairaer02@gmail.com>
2021-11-26 19:30:55 +0300

    Adding owner and Pet Page and controller Closes #13.

--- spring-pet-clinic-web/src/main/java/com/samrit/springpetclinic/controllers/PetController.java
+++ spring-pet-clinic-web/src/main/java/com/samrit/springpetclinic/controllers/PetController.java
@@ -2,12 +2,12 @@ package com.samrit.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/vets")
+@RequestMapping("/pets")
 @Controller
-public class VetController {
-    @RequestMapping({"/","/index","/index.html"})
-    public  String listVets(){
-        return "vets/index";
+public class PetController  {
+    @RequestMapping({"/", "/index", "/index.html"})
+    public String ListOfPets(){
+        return "pets/index";
+
     }
 }
