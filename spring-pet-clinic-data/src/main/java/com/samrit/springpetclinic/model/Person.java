35e906afbb1240c7b9de66fc8218dfa45cf05c8b
Samrit <hairaer02@gmail.com>
2021-11-26 07:50:25 +0300

    Adding BaseEntity Closes #10

--- spring-pet-clinic-data/src/main/java/com/samrit/springpetclinic/model/Person.java
+++ spring-pet-clinic-data/src/main/java/com/samrit/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.samrit.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String fristName;
     private String lastName;
 
--- spring-pet-clinic-data/src/main/java/com/samrit/springpetclinic/model/Pet.java
+++ spring-pet-clinic-data/src/main/java/com/samrit/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.samrit.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     PetType petType;
     Owner owner;
     LocalDate birthDate;
