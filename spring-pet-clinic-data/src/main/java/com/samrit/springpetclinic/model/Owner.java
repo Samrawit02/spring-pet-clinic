ff7f2eeddd98f7c21c1633b7324028b3e25def8f
Samrit <hairaer02@gmail.com>
2021-12-03 13:04:53 +0300

    Adding contact info properties for Owner Closes #28

--- spring-pet-clinic-data/src/main/java/com/samrit/springpetclinic/model/Owner.java
+++ spring-pet-clinic-data/src/main/java/com/samrit/springpetclinic/model/Owner.java
@@ -3,13 +3,40 @@ package com.samrit.springpetclinic.model;
 import java.util.Set;
 
 public class Owner extends  Person{
-    private Set<Pet> ptes;
+    private String address;
+    private String city;
+    private String telephone;
+    private Set<Pet> pets;
 
-    public Set<Pet> getPtes() {
-        return ptes;
+    public String getAddress() {
+        return address;
     }
 
-    public void setPtes(Set<Pet> ptes) {
-        this.ptes = ptes;
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
