64d84bcdff8c0027e7a787d7e92d99d04726ac65
Samrit <hairaer02@gmail.com>
2021-12-03 12:54:11 +0300

    Adding visits, update owner with pet relationships Closes #26

--- spring-pet-clinic-data/src/main/java/com/samrit/springpetclinic/model/Owner.java
+++ spring-pet-clinic-data/src/main/java/com/samrit/springpetclinic/model/Owner.java
@@ -1,4 +1,15 @@
 package com.samrit.springpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends  Person{
+    private Set<Pet> ptes;
+
+    public Set<Pet> getPtes() {
+        return ptes;
+    }
+
+    public void setPtes(Set<Pet> ptes) {
+        this.ptes = ptes;
+    }
 }
