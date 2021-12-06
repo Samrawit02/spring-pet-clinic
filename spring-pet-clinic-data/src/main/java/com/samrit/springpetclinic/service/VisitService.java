88bf4b088e7e21a08a679c6ee6fa56831295e7c8
Samrit <hairaer02@gmail.com>
2021-12-06 13:47:53 +0300

    Implemented Map impl for Visit Service Close #46

---
+++ spring-pet-clinic-data/src/main/java/com/samrit/springpetclinic/service/VisitService.java
@@ -0,0 +1,6 @@
+package com.samrit.springpetclinic.service;
+
+import com.samrit.springpetclinic.model.Visit;
+
+public interface VisitService extends CrService<Visit, Long>{
+}
---
+++ spring-pet-clinic-data/src/main/java/com/samrit/springpetclinic/service/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package com.samrit.springpetclinic.service.map;
+
+import com.samrit.springpetclinic.model.Visit;
+import com.samrit.springpetclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+
+    private final VisitService visitService;
+
+    public VisitMapService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.findById(id);
+    }
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet()==null || visit.getPet().getOwner() == null ||visit.getPet().getId()==null
+                || visit.getPet().getOwner().getId()==null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+    @Override
+    public Visit findById(Long Id) {
+        return super.findById(Id);
+    }
+}
